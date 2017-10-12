985a58783a0d4437455fa1300e4c74c7c567ffcd
jjunchen <jchen0318@gmail.com>
2017-10-11 19:53:34 -0700

    Complete linkedList & tree. End day 1 of sprint

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,35 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = new Node(value);
+    if (list.head === null) {
+      list.head = node;
+      list.tail = node;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var headVal = list.head.value;
+    list.head = list.head.next;
+    return headVal;
   };
 
   list.contains = function(target) {
+    var findTarget = function (node) {
+      if (node.value === target) {
+        return true;
+      } else {
+        if (node.next !== null) {
+          return findTarget(node.next);
+        } else {
+          return false;
+        }
+      } 
+    };
+    return findTarget(list.head);
   };
 
   return list;
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,34 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = new Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  var res = false;
+  var findTarget = function(child) {
+    if (child.value === target) {
+      res = true;
+    } else {
+      if (child.children) {
+        for (let i of child.children) {
+          console.log(i);
+          findTarget(i);
+        }
+      }
+    }
+  };
+  findTarget(this);
+  return res;
 };
 
 
